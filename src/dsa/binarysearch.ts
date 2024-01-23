7b0c868c78fe9e18ae7306ed5a1cfba3a90ec3d4
daveanthonyc <daveanthonyc@gmail.com>
2024-01-23 12:56:20 +1100

    first commit

---
+++ src/dsa/binarysearch.ts
@@ -0,0 +1,27 @@
+function binarySearch(arr: number[], target: number) {
+    let start = 0;
+    let end = arr.length-1;
+
+    if (target < arr[start]) {
+        return false;
+    }
+
+    while (start <= end) {
+        const middle = start + Math.floor((end - start) / 2);
+
+
+        if (arr[middle] === target) {
+            return true;
+        } else if (target < arr[middle]) {
+            // check left
+            end = middle;
+        } else {
+            // check right
+            start = middle+1;
+        }
+    }
+
+    return false;
+}
+
+export default binarySearch;
---
+++ src/dsa/recursion.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest'
+import binarySearch from './binarysearch'
+
+test('corner cases work in odd length array', () => {
+})
