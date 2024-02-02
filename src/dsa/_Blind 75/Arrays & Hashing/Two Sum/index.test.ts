25905cf06d176f585361b8dc11e0dbf5ce5b7b0e
daveanthonyc <daveanthonyc@gmail.com>
2024-02-02 14:07:47 +1100

    Blind 75: Array - completed two sum and find duplicate

--- src/dsa/_Blind 75/Arrays & Hashing/Two Sum/index.test.ts
+++ src/dsa/_Blind 75/Arrays & Hashing/Two Sum/index.test.ts
@@ -1,12 +1,13 @@
 import { expect, test } from "vitest";
-import isValidAnagram from ".";
+import twoSum from ".";
 
 test('returns empty array for array of one value', () => {
-    expect(isValidAnagram([1], 5)).toBe(false);
+    expect(twoSum([1], 5)).toEqual([]);
+    expect(twoSum([], 5)).toEqual([]);
 })
 
 test('works', () => {
-    expect(isValidAnagram([1,4,10,15,2,16], 6)).toEqual([1,4]);
+    expect(twoSum([1,4,10,15,2,16], 6)).toEqual([1,4]);
 })
 
 
--- src/dsa/_Blind 75/Arrays & Hashing/Two Sum/index.ts
+++ src/dsa/_Blind 75/Arrays & Hashing/Two Sum/index.ts
@@ -4,7 +4,7 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 */
 
-export default function isValidAnagram(nums: number[], target: number): number[] {
+export default function twoSum(nums: number[], target: number): number[] {
     const map = new Map();
 
     for (let i = 0; i < nums.length; i++) {
